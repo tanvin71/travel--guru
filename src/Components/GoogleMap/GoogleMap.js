d0c56efaf43dba4836924a03a5cfb94967a82145
tanvin71 <fakrultanvin320@gmail.com>
2020-09-24 20:25:51 +0600

    change again

--- src/Components/GoogleMap/GoogleMap.js
+++ src/Components/GoogleMap/GoogleMap.js
@@ -3,8 +3,17 @@ import React,{Component, useContext} from 'react';
 import { UserContext } from '../../App';
 export class GoogleMap extends Component {
   render() {
+    const style = {
+        width: '100%',
+        height: '100%'
+      }
+      const containerStyle = {
+        position: 'relative',  
+        width: '100%',
+        height: '100%'
+      }
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map  containerStyle ={containerStyle} style={style} google={this.props.google} zoom={14}>
  
         <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
